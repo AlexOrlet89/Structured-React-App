093213a7434b0c5f4aff7bfbb8a60f331732c225
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-17 16:16:40 -0700

    login form and signinsignup conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import Home from './views/Home';
+import Login from './views/Login';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Home />
+      <Login />
+    </>
+  );
 }
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [authType, setAuthType] = useState('Sign In');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSetAuthType = (type) => {
+    setAuthType(type);
+  };
+
   return (
     <>
-      <div>Login</div>;
+      <div>
+        <h2 onClick={() => handleSetAuthType('Sign In')}>Sign In</h2>
+        <h2 onClick={() => handleSetAuthType('Sign Up')}>Sign Up</h2>
+      </div>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button>{authType}</button>
+      </form>
     </>
   );
 }
