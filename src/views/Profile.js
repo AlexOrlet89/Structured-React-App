d6f8d7e5a9419b17c804437972c166cdc545353e
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-18 16:37:58 -0700

    validated contact rendering

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useContactContext } from '../context/ContactContext';
 
 export default function Profile() {
+  const { contacts, addContact } = useContactContext();
+  const [name, setName] = useState('');
+
+  const formSubmitter = (e) => {
+    e.preventDefault();
+    addContact(name);
+  };
+
+  console.log(contacts);
+
   return (
     <>
       <div>Profile</div>
+      <form onSubmit={formSubmitter}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      <div>
+        {contacts.map((contact) => (
+          <p key={contact.created_at}>{contact.name}</p>
+        ))}
+      </div>
     </>
   );
 }
